1e75e3e354fa90adbd087a35814a1bacab9a8834
jake-sager <sagerjake+gh@gmail.com>
2026-01-02 21:46:17 -0500

    Initial Commit

---
+++ Zazen/ZazenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZazenApp.swift
+//  Zazen
+//
+//  Created by Jake Sager on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ZazenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
